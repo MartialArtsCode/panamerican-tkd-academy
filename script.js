d796de5822905b0ae2a32b9583fa566bdaa64b1f
MartialArtsDevelopmentLab <ptacademy@duck.com>
2026-03-30 01:27:44 -0400

    Update script.js
    Signed-off-by: MartialArtsDevelopmentLab <ptacademy@duck.com>

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function initSocketIO() {
 
             // Authentication (mock)
             loginBtn.addEventListener('click', () => openModal());
-            closeModalBtn.addEventListener('click', () => closeModal());
+            modalCloseBtn.addEventListener('click', () => closeModal());
             loginModal.addEventListener('click', (e) => {
                 if (e.target === loginModal) closeModal();
             });
